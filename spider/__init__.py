6cfb7a54ef8adfa039a4d52c7af318be3385ca1a
code-football-dog <2472645764@qq.com>
2018-10-13 10:44:22 +0800

    add db

--- spider/__init__.py
+++ spider/__init__.py
@@ -4,7 +4,13 @@
 @time  2018-10-12
 @desc
 """
+
 import logging
+
+from lib.logger_magic import initlog_file
+from lib.db.mysql_basic import get_session
+
+initlog_file()
 logger = logging.getLogger()
 
 
@@ -12,6 +18,7 @@ class Spider:
 
     def __init__(self):
         self.logger = logger
+        self.db_session = get_session()
 
     def parse(self):
         pass
